fe4f45af44896849b28c045bee59c2ce3f84f4af
99002678 <71488043+99002678@users.noreply.github.com>
2020-09-19 12:53:52 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,31 +1,68 @@
-#include<calculator.h>
+#include "calculator.h"
+
 int main()
 {
-char operand;
+int operation;
+int number1, number2;
+printf("1. Addition\n");
+printf("2. Subtraction\n");
+printf("3. Multiplication\n");
+printf("4. Division\n");
+printf("5. Modulus\n");
+printf("6. Power\n");
+printf("7. Factorial\n");
+	
+scanf("%d, &operation);
 
 switch(operand)
 {
-    case 1: addition(int number1,int number2);
+       case 1:  printf("Enter first operand\n");
+	        scanf("%d", &number1);
+	        printf("Enter second operand\n");
+	        scanf("%d", &number2);
+	        printf("Result is:%d\n",addition(number1,number2));
 	        break;
     
-	case 2: subtraction(int number1,int number2);
+	case 2: printf("Enter first operand\n");
+	        scanf("%d", &number1);
+	        printf("Enter second operand\n");
+	        scanf("%d", &number2);
+	        printf("Result is:%d\n",subtraction(number1,number2));
 	        break;
 			
-	case 3: multiplication(int number1,int number2);
+	case 3: printf("Enter first operand\n");
+	        scanf("%d", &number1);
+	        printf("Enter second operand\n");
+	        scanf("%d", &number2);
+	        printf("Result is:%d\n",multiplication(number1,number2));
 	        break;
 			
-	case 4: division(int number1,int number2);
+	case 4: printf("Enter first operand\n");
+	        scanf("%d", &number1);
+	        printf("Enter second operand\n");
+	        scanf("%d", &number2);
+	        printf("Result is:%d\n",division(number1,number2));
 	        break;
 			
-	case 5: modulus(int number1,int number2);
+	case 5: printf("Enter first operand\n");
+	        scanf("%d", &number1);
+	        printf("Enter second operand\n");
+	        scanf("%d", &number2);
+	        printf("Result is:%d\n",addition(number1,number2));
 	        break;
 			
-	case 6: power(double number1,double number2);
+	case 6: printf("Enter first operand\n");
+	        scanf("%d", &number1);
+	        printf("Enter second operand\n");
+	        scanf("%d", &number2);
+	        printf("Result is:%d\n",power(number1,number2));
 	        break;
 			
-	case 7: factorial(int number);
-	         break;
+	case 7: printf("Enter a number\n");
+	        scanf("%d", &number1);
+	        printf("Result is:%d\n",factorial(number1));
+	        break;
 			 
-	default: printf("Invalid operation\n");
 }
+      return 0;
 }	
