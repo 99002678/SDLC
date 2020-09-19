84d520a4127d24d61ca8dfb9ede2013ab583f542
99002678 <71488043+99002678@users.noreply.github.com>
2020-09-19 06:46:03 +0530

    Add files via upload

---
+++ inc/calculator.h
@@ -0,0 +1,15 @@
+#ifndef calculator.h
+#define calculator.h
+
+#include<stdio.h>
+#include<nath.h>
+
+int addition(int number1,int number2);
+int subtraction(int number1,int number2);
+int multiplication(int number1,int number2);
+int division(int number1,int number2);
+int modulus(int number1,int number2);
+double power(double number,double power);
+double factorial(int number);
+ 
+#endif
