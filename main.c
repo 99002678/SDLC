d4ca24fbccbdb000b840ae86824aabedf8d558d8
99002678 <71488043+99002678@users.noreply.github.com>
2020-09-19 06:42:30 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,29 @@
+#include<calculator.h>
+char operand;
+
+switch(operand)
+{
+    case 1: addition(int number1,int number2);
+	        break;
+    
+	case 2: subtraction(int number1,int number2);
+	        break;
+			
+	case 3: multiplication(int number1,int number2);
+	        break;
+			
+	case 4: division(int number1,int number2);
+	        break;
+			
+	case 5: modulus(int number1,int number2);
+	        break;
+			
+	case 6: power(double number1,double number2);
+	        break;
+			
+	case 7: factorial(int number);
+	         break;
+			 
+	default: printf("Invalid operation\n");
+}
+	
